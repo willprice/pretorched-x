d9222fd29ac215e65a668442528c2f9efac49b43
Will Price <will.price94@gmail.com>
2019-06-24 23:26:43 +0100

    Add hubconf

---
+++ hubconf.py
@@ -0,0 +1,15 @@
+dependencies = ['torch', 'pretorched']
+import pretorched
+
+
+def resneti3d50(num_classes: int = 400, pretrained: str = 'moments', *args, **kwargs):
+    """
+    Args:
+        num_classes: Number of classes of classification layer
+        pretrained: Either 'moments' or 'kinetics-400'.
+
+    Returns:
+        Inflated 3D ResNet-50
+    """
+
+    return pretorched.resneti3d50(num_classes=num_classes, pretrained=pretrained, *args, **kwargs)
